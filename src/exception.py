69986eea46b3698e8a774e86cc13428760418bb4
Mohammad Zaki Gundagi <md.zaki.gundagi@gmail.com>
2023-03-09 14:41:27 +0530

    changed to vs code

--- src/exception.py
+++ src/exception.py
@@ -1,4 +1,6 @@
 import sys
+from src.logger import logging
+
 
 def error_message_detail(error,error_detail:sys):
     _,_,exc_tb=error_detail.exc_info()
@@ -15,4 +17,3 @@ class CustomException(Exception):
 
     def __str__(self):
         return self.error_message
-
