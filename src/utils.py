a40bc339504fdbd2e0f24dcbc3ad384b804f07c7
Mohammad Zaki Gundagi <md.zaki.gundagi@gmail.com>
2023-03-21 15:15:25 +1100

    Prediction pipeline and Flask app

--- src/utils.py
+++ src/utils.py
@@ -9,6 +9,8 @@ from src.exception import CustomException
 
 from sklearn.metrics import r2_score
 
+#write in util.py because these are common functionalities across the files
+
 def save_object(file_path, obj):
     try:
         dir_path = os.path.dirname(file_path)
@@ -42,5 +44,14 @@ def evaluate_models(X_train, y_train,X_test,y_test,models):
 
         return report
     
+    
+    except Exception as e:
+        raise CustomException(e, sys)
+    
+def load_object(file_path):
+    try:
+        with open(file_path, "rb") as file_obj:
+            return dill.load(file_obj)
+        
     except Exception as e:
         raise CustomException(e, sys)
